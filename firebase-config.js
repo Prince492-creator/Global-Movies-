f620b0b88f66f02374678683a0a20f37bf9f6b2f
Prince492-creator <ianhorn2018@gmail.com>
2025-05-23 18:31:46 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC26bGAFccmsiDsrrqXmSGiSxGUlE8RkRs",
+  authDomain: "globalmovies.firebaseapp.com",
+  projectId: "Global_Movies ",
+  appId: "global-movies-9ba71"
+};
+
+firebase.initializeApp(firebaseConfig);
